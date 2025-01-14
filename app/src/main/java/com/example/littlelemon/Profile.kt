aeaf335d368e8ecd3c39b3661b5a008ba75b71d0
unknown <zhapalakov@gmail.com>
2025-01-14 20:13:55 +0500

    1

---
+++ app/src/main/java/com/example/littlelemon/Profile.kt
@@ -0,0 +1,15 @@
+package com.example.littlelemon
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Profile(){}
+
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun ProfileScreen(){
+    Profile()
+}
